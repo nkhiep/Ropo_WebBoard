d46ac8b71b5f4a2d6adf89fdd7ee487cfb74065f
nkhiep <hiepnk26@gmial.com>
2023-02-19 16:04:08 +0700

    video 14, 15, 16

--- boards/views.py
+++ boards/views.py
@@ -1,9 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
-from .models import Board
+from .models import Board, Topic, Post
+from .forms import NewTopicForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards':boards})
     
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board':board})
+
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    
+    if request.method == 'POST':
+        user = User.objects.first()
+        form = NewTopicForm(request.POST)
+        
+        if form.is_valid():
+            topic = form.save(commit=False)
+            
+            topic.board = board
+            topic.starter = user
+            topic.save()
+        
+            post = Post.objects.create(
+                message = form.cleaned_data.get('message'),
+                topic = topic,
+                created_by = user,
+                update_by = user
+            )
+        
+            return redirect('board_topics', pk=board.pk)
+    
+    else:
+        form = NewTopicForm()
+    
+    print(form.errors)
+    
+    return render(request, 'new_topic.html', {'board':board, 'form':form})
